bbd6d7a63b7051a86d2849b154f29604f2c463c4
Leonomics <leocv97@gmail.com>
2022-09-21 15:47:03 +0200

    improve snack-1

--- snack-1.php
+++ snack-1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>snak 1</title>
+    <title>snack 1</title>
 </head>
 <body>
 
@@ -22,7 +22,7 @@
     <a href="index.php">Index Page</a>
     <ul>
     <?php
-        for($i=0;$i<count($partite); $i++){
+        for($i=0; $i<count($partite); $i++){
             $match = $partite[$i];
             ?>
             <li>
